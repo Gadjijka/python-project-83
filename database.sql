d3fd58e2ae9ad0d2f0072a627c0649bba238e76c
Gajijka <galimovgadjimurad2020@gmail.com>
2024-05-09 16:39:16 +0300

    Changing database

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar UNIQUE NOT NULL,
+    created_at date
+);
+
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code bigint,
+    h1 varchar,
+    title varchar,
+    description varchar,
+    created_at date
+);
