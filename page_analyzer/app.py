e7d849d46fff9dd761804ff69b4739850f7882c0
Gajijka <galimovgadjimurad2020@gmail.com>
2024-05-09 17:13:43 +0300

    asd

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -15,6 +15,7 @@ from .html import parse_page
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
 
 
 @app.get('/')
