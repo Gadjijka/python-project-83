20e4fb710fad007c135a4ca093167735524129e5
Gajijka <galimovgadjimurad2020@gmail.com>
2024-05-10 02:30:38 +0300

    Fixing check_urls

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -65,7 +65,7 @@ def url_checks(id):
     try:
         response = requests.get(url.name)
         response.raise_for_status()
-    except requests.exceptions.RequestExceprion:
+    except requests.exceptions.RequestException:
         flash('Произошла ошибка при проверке', 'alert-danger')
         return redirect(url_for('get_url', id=id))
     check_data = parse_page(response.text)
