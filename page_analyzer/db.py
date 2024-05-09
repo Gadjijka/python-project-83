abf656acd6c7cac49085fc964afdc38f97729bc2
Gajijka <galimovgadjimurad2020@gmail.com>
2024-05-09 22:19:51 +0300

    as

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -35,7 +35,7 @@ class DatabaseConnection:
     def add_url_into_db(url):
         with DatabaseConnection() as cursor:
             query = (
-                'INSERT INTO url '
+                'INSERT INTO urls '
                 '(name, created_at) '
                 'VALUES (%s, %s) '
                 'RETURNING id'
@@ -46,14 +46,14 @@ class DatabaseConnection:
 
     def get_url_by_name(url):
         with DatabaseConnection() as cursor:
-            query = 'SELECT * FROM url WHERE name = (%s)'
+            query = 'SELECT * FROM urls WHERE name = (%s)'
             cursor.execute(query, (url,))
             data = cursor.fetchone()
             return data
 
     def get_url_by_id(id):
         with DatabaseConnection() as cursor:
-            query = 'SELECT * FROM url WHERE id = (%s)'
+            query = 'SELECT * FROM urls WHERE id = (%s)'
             cursor.execute(query, (id,))
             data = cursor.fetchone()
             return data
@@ -89,16 +89,16 @@ class DatabaseConnection:
         with DatabaseConnection() as cursor:
             query = (
                 'SELECT '
-                'url.id AS id '
-                'url.name AS name, '
+                'urls.id AS id '
+                'urls.name AS name, '
                 'url_checks.created_at AS las_check, '
                 'status_code '
-                'FROM url '
+                'FROM urls '
                 'LEFT JOIN url_checks '
-                'ON url.id = url_checks.url_id '
+                'ON urls.id = url_checks.url_id '
                 'AND url_checks.id = ('
                 'SELECT max(id) FROM url_checks'
-                'WHERE url.id = url_checks.url_id '
+                'WHERE urls.id = url_checks.url_id '
                 'ORDER BY url.id DESC;'
             )
             cursor.execute(query)
